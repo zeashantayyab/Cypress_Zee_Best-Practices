ffbcfbf65472940f8a5da306c050ab536db3d1df
zeashantayyab <47562647+zeashantayyab@users.noreply.github.com>
2026-03-06 07:45:45 +0400

    Add files via upload

--- 5-commands.js
+++ 5-commands.js
@@ -32,12 +32,21 @@ Cypress.Commands.add('getText', {prevSubject: 'element'}, ($element) => {
     return cy.wrap($element).invoke('text')
 })
 
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('#email').type(email)
+    cy.get('#login-password').type(password)
+    cy.get('#login').click()
+    cy.get('#dropdownMenu1').should('exist')
+})
 
+Cypress.Commands.add('logout', () => {
+    cy.get('#dropdownMenu1').click()
+    cy.contains('Logout').click()
+    cy.get('a[href="/login"]').should('have.text', 'Sign In')
+})
 
-
-
-
-
-
-
-
+Cypress.Commands.add('searchCourse', (category, courseString) => {
+    cy.get('select[name="categories"]').select(category)
+    cy.get('input[placeholder="Search Course"]').type(courseString)
+    cy.get('button[class="find-course search-course"]').click()
+})
